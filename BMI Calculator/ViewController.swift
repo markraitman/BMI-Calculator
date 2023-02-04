0f028c55d1779b58628772ae664c3f56c830a9ec
Mark Raitman <markraitman@mail.ru>
2023-02-04 23:11:07 +0300

    Добавлены картинки для экранов, вьюшке и т.д., также иконки для приложения и заставка лаунч-скрина. Подведены аутлеты для слайдеров

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    @IBAction func heghtSliderChanged(_ sender: UISlider) {
+        print(String(format: "%.2f", sender.value))
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        print(String(format: "%.0f", sender.value))
+    }
+    
 
 }
 
